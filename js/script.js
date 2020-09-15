0282b3506c0421aef9d6a1743a7c1c2b7174f450
Victoria <kapitonenko.vick@ya.ru>
2020-09-15 21:06:09 +0300

    feat: check text for start

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@ var app = new Vue({
   el: '#app',
   data() {
     return {
-      text: null,
+      text: '',
       isRun: false,
       currentWord: null,
-      wordArray: [],
       interval: null,
       speed: 200,
       indexOfArray: 0,
@@ -19,24 +18,27 @@ var app = new Vue({
     },
     progress() {
       return ((this.indexOfArray) / this.wordArray.length) * 100;
+    },
+    wordArray() {
+      return this.text.split(' ');
     }
   },
   methods: {
     reset() {
-      this.text = null;
+      this.text = '';
       this.isRun = false;
       this.currentWord = null;
-      this.wordArray = [];
       this.interval = null;
       this.indexOfArray = 0;
       this.isPause = false;
       this.isEnd = false;
     },
     start() {
-      this.isRun = true;
-      this.wordArray = this.text.split(' ');
-      this.indexOfArray = 0;
-      this.run();
+      if (this.text.length > 0) {
+        this.isRun = true;
+        this.indexOfArray = 0;
+        this.run();
+      }
     },
     run() {
       this.isEnd = false;
